4371d44b9b8bdb35c71ccf77cdabfee307ce04ca
Арслан Магомедович <pasbaniki@gmail.com>
2024-02-13 20:18:47 +0300

    Добавьте файлы проекта.

---
+++ RoundingError/Program.cs
@@ -0,0 +1,20 @@
+﻿using var input = new StreamReader(Console.OpenStandardInput());
+using var output = new StreamWriter(Console.OpenStandardOutput());
+
+int setAll = int.Parse(input.ReadLine());
+for (int i = 0; i < setAll; i++)
+{
+    var s = input.ReadLine().Split(' ');
+    double p = double.Parse(s[1]);
+
+    double commissionAll = 0;
+    for (int j = 0; j < int.Parse(s[0]); j++)
+    {
+        int a = int.Parse(input.ReadLine());
+        double oneCommission = a * (p / 100);
+        oneCommission -= Math.Truncate(oneCommission);
+        commissionAll += oneCommission;
+    }
+
+    output.WriteLine(commissionAll % 1 == 0 ? "0.00" : Math.Round(commissionAll, 2).ToString("F2").Replace(',', '.'));
+}
